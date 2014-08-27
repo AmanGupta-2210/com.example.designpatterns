e425d2385c807184ed86ee9e5bafb55c4ddbac23
Aman Gupta <aman.tit10@gmail.com>
2014-08-27 09:37:44 +0530

    Correct Comments

--- com.example.designpatterns/src/main/java/com/example/pattern/builder/package-info.java
+++ com.example.designpatterns/src/main/java/com/example/pattern/builder/package-info.java
@@ -1,7 +1,7 @@
 /**
  * @author Aman Gupta (aman.tit10@gmail.com)
  * @version 1.0
- * Design Pattern : Factory
+ * Design Pattern : Builder
  * Description : A Builder class builds the final object step by step. This builder is independent of other objects.
  */
 package com.example.pattern.builder;
